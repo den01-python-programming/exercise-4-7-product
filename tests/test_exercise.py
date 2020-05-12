dfe45577513983d33712ae54af917cca3f39862d
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 12:10:24 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,7 +3,7 @@ from src import product
 
 def test_exercise(capsys):
     sausages = product.Product("sausage",1.2,14)
-    print(sausages.print_product)
+    sausages.print_product()
 
     out, err = capsys.readouterr()
     assert out == "sausage, price 1.2, 14 pcs\n"
